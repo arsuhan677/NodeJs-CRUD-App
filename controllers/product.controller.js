893494295da3648aeee912fd99d9822880bcf486
AR Suhan <aminurrahmansuhan@gmail.com>
2026-01-06 00:29:41 +0600

    update for middleware

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,26 +1,75 @@
 const Product = require("../model/product.model");
 
 const getProducts = async (req, res) => {
-    try {
-        const products = await Product.find({});
-    
-        res.status(200).json(products);
-      } catch (error) {
-        res.status(500).json({ message: error.message });
-      }
+  try {
+    const products = await Product.find({});
+
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const getProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createProduct = async (req, res) => {
+  try {
+    const { name, price, quantity, image } = req.body;
+
+    const product = await Product.create({
+      name,
+      price,
+      quantity,
+      image,
+    });
+    res.status(201).json({ success: true, data: product });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+const updateProduct = async (req, rse) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res.status(404).json({ message: "Product is not found" });
+    }
+    const updateProduct = await Product.findById(id);
+    res.status(200).json(updateProduct);
+  } catch (error) {
+    res.status(200).json({ message: error.message });
+  }
+};
+
+const deleteProduct = async (req, res) => {
     try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-      } catch (error) {
-        res.status(500).json({ message: error.message });
-      }
+    
+            const {id} = req.params;
+            const product = await Product.findByIdAndDelete(id)
+            if (!product) {
+                return res.status(404).json({message: "Product in not foun"})  
+            }
+            res.status(200).json({message: "Product delete successfully!"})
+            
+        } catch (error) {
+            res.status(200).json({message: error.message})
+            
+        }
 }
 
 module.exports = {
-    getProducts,
-    getProduct
-}
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct
+};
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const Product = require("../model/product.model");
 const router = express.Router();
-const { getProducts, getProduct } = require("../controllers/product.controller")
+const { getProducts, getProduct, createProduct, updateProduct, deleteProduct } = require("../controllers/product.controller")
 
 
 
 router.get("/", getProducts)
 
 router.get("/:id", getProduct)
+
+router.post("/", createProduct)
+
+// update api
+
+router.put("/:id", updateProduct)
+
+// delete api
+
+router.delete("/:id", deleteProduct)
 
 module.exports = router;
